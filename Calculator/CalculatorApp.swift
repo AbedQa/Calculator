a7b4cf27bb6d12d9874150646012bfff40a7633a
Abdelrahman Qasim <abdelrahman.qasim@orange.com>
2022-01-12 15:58:36 +0200

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by AbdelrahmanQasim on 1/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
