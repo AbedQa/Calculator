8d49645708aaeae6660835b1c26142ea1a7db482
Abdelrahman Qasim <abdelrahman.qasim@orange.com>
2022-01-12 20:33:21 +0200

    add all files

---
+++ Calculator/View/CalcButtons.swift
@@ -0,0 +1,20 @@
+//
+//  CalcButtons.swift
+//  Calculator
+//
+//  Created by AbdelrahmanQasim on 1/12/22.
+//
+
+import SwiftUI
+
+struct CalcButtons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalcButtons_Previews: PreviewProvider {
+    static var previews: some View {
+        CalcButtons()
+    }
+}
